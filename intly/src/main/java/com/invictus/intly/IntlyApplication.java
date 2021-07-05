4a4313f066fe22d6374838fc0c657c3c398c548d
hyeju1123 <hjjun1123@gmail.com>
2021-07-05 15:46:58 +0900

    first commit

---
+++ intly/src/main/java/com/invictus/intly/IntlyApplication.java
@@ -0,0 +1,13 @@
+package com.invictus.intly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntlyApplication.class, args);
+	}
+
+}
---
+++ intly/src/test/java/com/invictus/intly/IntlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.invictus.intly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
